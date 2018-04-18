7857083d27c188780f3262526cabe59d432d127b
Matilda Rutherford <rutherford.matilda@gmail.com>
2018-04-18 12:52:39 +1000

    update

--- main.rb
+++ main.rb
@@ -1,7 +1,8 @@
 
 require 'sinatra'
-require 'sinatra/reloader'
-require 'pry'
+# require 'sinatra/reloader'
+# require 'pry'
+require 'active_record'
 require_relative 'db_config'
 require_relative 'models/user'
 enable :sessions
