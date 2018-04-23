573eebec5157380e49d96cc33576254bb5a5ee6a
Matilda Rutherford <rutherford.matilda@gmail.com>
2018-04-23 13:32:11 +1000

    fix error

--- models/asset.rb
+++ models/asset.rb
@@ -1,5 +1,22 @@
+require 'carrierwave'
+require 'carrierwave/orm/activerecord'
+require 'cloudinary'
 
+class ImageUploader < CarrierWave::Uploader::Base
+  include Cloudinary::CarrierWave
+
+    process :convert => 'png'
+    process :tags => ['post_picture']
+
+    version :standard do
+      process :eager => true
+      process :resize_to_fit => [350, 350, :north]
+    end
+
+
+end
 
 class Asset < ActiveRecord::Base
   mount_uploader :image, ImageUploader
+  belongs_to :user
 end
